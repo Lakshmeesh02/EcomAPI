4a0438b86bed8c48e0bfe193b07c4d3bf04a2771
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2024-08-09 09:35:39 +0530

    user schema for db

---
+++ src/models/user.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+
+const userschema=new mongoose.Schema({
+    username: {type: String, required: true}, 
+    password: {type: String, required: true}, 
+
+})
+
+const user=mongoose.model('User', userschema)
+module.exports=user;
