a2ae1e3f0ff61e7f1fd46c995e5d3796efc79b44
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2024-08-10 11:00:17 +0530

    query and mutation for orders

--- src/schemas/graphqlschemas.js
+++ src/schemas/graphqlschemas.js
@@ -1,6 +1,7 @@
-const { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLFloat, GraphQLList } = require('graphql');
+const { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLFloat, GraphQLList, GraphQLInt } = require('graphql');
 const ProductModel = require('../models/product');
 const UserModel=require('../models/user');
+const OrdersModel=require('../models/orders');
 
 const productType = new GraphQLObjectType({
     name: 'Product',
@@ -22,6 +23,26 @@ const userType = new GraphQLObjectType({
     }),
 })
 
+const orderType = new GraphQLObjectType({
+    name: 'Order', 
+    fields: ()=> ({
+        id: {type: GraphQLString}, 
+        userId: {type: GraphQLString}, 
+        products: {
+            type: new GraphQLList(
+                new GraphQLObjectType({
+                    name: 'OrderProduct',
+                    fields: ()=> ({
+                        productId: {type: GraphQLString}, 
+                        quantity: {type: GraphQLFloat}
+                    }),
+                })
+            )
+        },
+        totalamount: {type: GraphQLFloat}
+    })
+})
+
 const rootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -31,6 +52,7 @@ const rootQuery = new GraphQLObjectType({
                 return ProductModel.find(); // Fetch all products
             },
         },
+
         productByCategory: {
             type: new GraphQLList(productType),
             args: { category: { type: GraphQLString } },
@@ -38,17 +60,42 @@ const rootQuery = new GraphQLObjectType({
                 return ProductModel.find({category: args.category}); // Fetch a single product by ID
             },
         },
+
+        productById: {
+            type: productType,
+            args: {id: {type: GraphQLString}}, 
+            resolve(parent, args) {
+                return ProductModel.findById(args.id)
+            }
+        },
+
         users: {
             type: new GraphQLList(userType),
             resolve(parent, args) {
                 return UserModel.find();
             },
         },
+
         userById: {
             type: userType,
             args: {id: {type: GraphQLString}},
             resolve(parent, args) {
                 return UserModel.findById(args.id)
+            },
+        },
+
+        orders: {
+            type: new GraphQLList(orderType),
+            resolve(parent, args) {
+                return OrdersModel.find();
+            },
+        },
+
+        orderByUserId: {
+            type: orderType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args) {
+                return OrdersModel.find({userId: args.userId})
             },
         },
     },
@@ -89,7 +136,35 @@ const mutation = new GraphQLObjectType({
                 })
                 return user.save()
             }
-        }
+        },
+
+        addOrder: {
+            type: orderType,
+            args: {
+                userId: {type: GraphQLString},
+                productId: {type: GraphQLString}, 
+                quantity: {type: GraphQLFloat}
+            },
+            async resolve(parent, args) {
+                const product=await ProductModel.findById(args.productId)
+                const itemtotalprice=product.price*args.quantity
+                let order=await OrdersModel.findOne({ userId: args.userId})
+
+                if(!order) {
+                    order=new OrdersModel({
+                        userId: args.userId,
+                        products: [],
+                        totalamount: 0
+                    })
+                }
+                order.products.push({
+                    productId: args.productId,
+                    quantity: args.quantity
+                })
+                order.totalamount+=itemtotalprice
+                return await order.save()
+            },
+        },
     },
 });
 
